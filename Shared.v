4b988caa28b0a8bb7f01419ceae7d00a61f3e496
Jakab Tamás <45921728+yebume@users.noreply.github.com>
2022-03-02 16:18:02 +0100

    Create Shared.v

---
+++ Shared.v
@@ -0,0 +1,37 @@
+Inductive Boole : Set :=
+  |igaz   : Boole
+  |hamis  : Boole
+  |lehet  : Boole.
+
+Definition és (x : Boole) (y : Boole) : Boole :=
+  match x with
+    |igaz => match y with
+      |igaz  => igaz
+      |hamis => hamis
+      |lehet => lehet
+    end
+    |hamis => hamis
+    |lehet => match y with
+      |igaz  => lehet
+      |hamis => hamis
+      |lehet => lehet
+    end
+  end.
+
+Definition vagy (x : Boole) (y : Boole) : Boole :=
+  match x with
+    |igaz   => igaz
+    |hamais => y
+    |lehet  => match y with
+      |igaz  => igaz
+      |hamis => lehet
+      |lehet => lehet
+    end
+ end.
+
+Definition nem (x : Boole) : Boole :=
+  match x with
+    |igaz  => hamis
+    |hamis => igaz
+    |lehet => lehet
+  end.
